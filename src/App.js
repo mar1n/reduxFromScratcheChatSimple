03dad9930d7948dab8860c8251dc8cda71b28937
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-25 23:36:17 +0100

    subscribing to the store, oberver pattern

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 function createStore(reducer, initialState) {
   let state = initialState;
-  
+  const listeners = []
+  const subscribe = (listener) => (
+    listeners.push(listener)
+  )
   const getState = () => (state);
 
   const dispatch = (action) => {
     state = reducer(state, action);
+    listeners.forEach(l => l())
   };
 
   return {
+    subscribe,
     getState,
     dispatch,
   };
@@ -39,26 +44,26 @@ const initialState = { messages: [] }
 
 const store = createStore(reducer, initialState)
 
+const listener = () => {
+  console.log('Current state: ')
+  console.log(store.getState())
+}
+
+store.subscribe(listener)
+
 const addMessageAction1 = {
   type: 'ADD_MESSAGE',
-  message: 'How does it look, Neil?',
+  message: 'How do you read?',
 }
 
 store.dispatch(addMessageAction1)
-const stateV1 = store.getState()
 
 const addMessageAction2 = {
   type: 'ADD_MESSAGE',
-  message: 'Looking good.',
+  message: 'I read you loud and clear, Houston.',
 }
 
 store.dispatch(addMessageAction2)
-const stateV2 = store.getState()
-
-console.log('State v1: ')
-console.log(stateV1)
-console.log('State v2: ')
-console.log(stateV2)
 
 const deleteMessageAction = {
   type: 'DELETE_MESSAGE',
@@ -66,7 +71,3 @@ const deleteMessageAction = {
 }
 
 store.dispatch(deleteMessageAction)
-const stateV3 = store.getState()
-
-console.log('State v3: ')
-console.log(stateV3)
